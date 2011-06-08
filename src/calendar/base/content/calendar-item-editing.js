139a1c36cb1b9b2cf4692fbb4b95a40dd35c248b
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2011-06-08 20:13:21 +0000

    Inverse-version specific: fixed isCalendarAvailable as in the previous patch
    Monotone-Parent: cd356e7def8851ba1f51f15389b508c150590faf
    Monotone-Revision: 9783bd93ad0d83fa204f789ab00ac6301e0bf03f
    
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2011-06-08T20:13:21

--- src/calendar/base/content/calendar-item-editing.js
+++ src/calendar/base/content/calendar-item-editing.js
@@ -288,6 +288,7 @@ function isCalendarAvailable(aCalendar) {
     return (!aCalendar.getProperty("disabled") &&
             !aCalendar.readOnly &&
             (!getIOService().offline ||
+             aCalendar.getProperty("cache.enabled") ||
              aCalendar.getProperty("requiresNetwork") === false));
 }
 
