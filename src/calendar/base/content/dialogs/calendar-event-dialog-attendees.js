f7004038bc083cce5479bb021f087ebb9969530d
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2011-10-21 19:58:33 +0000

    Temporary reversal of list expansion patch
    Monotone-Parent: 5c3814c2ba56609a196d439190e9bceac915df61
    Monotone-Revision: 99d5c085668b7b2a205e96d67c6438805be4c877
    
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2011-10-21T19:58:33
    Monotone-Branch: ca.inverse.calendar.tb3

--- src/calendar/base/content/dialogs/calendar-event-dialog-attendees.js
+++ src/calendar/base/content/dialogs/calendar-event-dialog-attendees.js
@@ -947,19 +947,12 @@ function onAttrModified(event) {
     var target = event.originalTarget;
     if (target.hasAttribute("anonid") &&
         target.getAttribute("anonid") == "input" &&
-        event.attrName == "focused") {
+        event.attrName == "focused" &&
+        event.newValue == "true") {
         var attendees = document.getElementById("attendees-list");
-        if (event.newValue == "true") {
-	  var grid = document.getElementById("freebusy-grid");
-            if (grid.firstVisibleRow != attendees.firstVisibleRow) {
-                grid.firstVisibleRow = attendees.firstVisibleRow;
-            }
-        } else {
-            if (!target.lastListCheckedValue
-                || target.lastListCheckedValue != target.value) {
-                attendees.resolvePotentialList(target);
-                target.lastListCheckedValue = target.value;
-            }
+        var grid = document.getElementById("freebusy-grid");
+        if (grid.firstVisibleRow != attendees.firstVisibleRow) {
+            grid.firstVisibleRow = attendees.firstVisibleRow;
         }
     }
 
